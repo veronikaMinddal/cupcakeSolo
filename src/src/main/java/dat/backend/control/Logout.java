59f74bc0a2e76e82542fa5812085503aaaa43c89
veronikat16 <71886093+veronikat16@users.noreply.github.com>
2022-11-16 13:09:16 +0100

    startkode sættes op

---
+++ src/src/main/java/dat/backend/control/Logout.java
@@ -0,0 +1,22 @@
+package src.main.java.dat.backend.control;
+
+import dat.backend.model.persistence.ConnectionPool;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "logout", urlPatterns = {"/logout"} )
+public class Logout extends HttpServlet
+{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        response.setContentType("text/html");
+        HttpSession session = request.getSession();
+        session.invalidate();
+        response.sendRedirect("index.jsp");
+    }
+}
